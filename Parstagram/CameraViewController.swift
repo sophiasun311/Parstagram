983595f5847075950e783cd0706f9f0e1088b6a9
Sophia <qyoung@ucsd.edu>
2019-04-08 19:15:07 -0700

    enable camera

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Sophia Sun on 4/8/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
